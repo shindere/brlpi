1c8a9bf28e2110c595b611bf9bd6064b87988220
Mario Lang <mlang@delysid.org>
2017-11-26 23:19:10 +0100

    Make lftp support libidn

--- external.mk
+++ external.mk
@@ -1,1 +1,8 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_BRLPI_PATH)/package/*/*.mk))
+
+ifeq ($(BR2_PACKAGE_LIBIDN),y)
+LFTP_DEPENDENCIES += libidn
+LFTP_CONF_OPTS += --with-libidn=$(STAGING_DIR)/usr
+else
+LFTP_CONF_OPTS += --without-libidn
+endif
