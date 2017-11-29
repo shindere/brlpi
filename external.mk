ac38202c2222194c098c68669ef5947e908966db
Mario Lang <mlang@delysid.org>
2017-11-29 22:45:07 +0100

    lftp libidn support was merged

--- external.mk
+++ external.mk
@@ -1,8 +1,1 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_BRLPI_PATH)/package/*/*.mk))
-
-ifeq ($(BR2_PACKAGE_LIBIDN),y)
-LFTP_DEPENDENCIES += libidn
-LFTP_CONF_OPTS += --with-libidn=$(STAGING_DIR)/usr
-else
-LFTP_CONF_OPTS += --without-libidn
-endif
