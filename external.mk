43e13ba8093e43b2900b7b112652f1936d2f4e2b
Mario Lang <mlang@delysid.org>
2017-12-01 18:28:08 +0100

    Fix syntax error

--- external.mk
+++ external.mk
@@ -2,6 +2,6 @@ include $(sort $(wildcard $(BR2_EXTERNAL_BRLPI_PATH)/package/*/*.mk))
 
 define BRLPI_CREATE_USER_OVERLAY
 	mkdir -p $(HOME)/.config/brlpi/overlay
-endif
+endef
 
 TARGET_FINALIZE_HOOKS += BRLPI_CREATE_USER_OVERLAY
