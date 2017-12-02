3baa5529067d9a4b597e5154e017485a14a35c41
Mario Lang <mlang@delysid.org>
2017-12-02 13:02:19 +0100

    package/lynx: Add support for SSL

--- package/lynx/lynx.mk
+++ package/lynx/lynx.mk
@@ -8,8 +8,15 @@ LYNX_VERSION = cur
 LYNX_SOURCE = lynx-$(LYNX_VERSION).tar.bz2
 LYNX_SITE = http://invisible-island.net/datafiles/release
 
-LYNX_DEPENDENCIES = ncurses
-
+LYNX_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES) ncurses
 LYNX_CONF_OPTS = --with-screen=ncurses --with-curses-dir=$(TARGET_DIR)/usr
+
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+LYNX_DEPENDENCIES += openssl
+LYNX_CONF_OPTS += --with-ssl
+else ifeq ($(BR2_PACKAGE_GNUTLS),y)
+LYNX_DEPENDENCIES += gnutls
+LYNX_CONF_OPTS += --with-gnutls
+endif
 
 $(eval $(autotools-package))
